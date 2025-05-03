fbc0d714cdc8480d48d0f84c90ead57b027269e2
Ashutosh Singh <singhashutosh1025@gmail.com>
2025-05-03 17:32:39 +0530

    made some component of frontend

--- Backend/app.js
+++ Backend/app.js
@@ -18,13 +18,13 @@ config({
   path: "./config/config.env",
 });
 
-// app.use(
-//   cors({
-//     origin: [process.env.FRONTEND_URL],
-//     methods: ["POST", "GET", "PUT", "DELETE"],
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
 app.use(express.json());
